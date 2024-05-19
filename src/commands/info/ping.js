1a88c97a6d962b517fd0b87ea0ca2f8e01be5069
dsastreux <167467152+dsastreux@users.noreply.github.com>
2024-05-19 14:53:25 +0200

    Update ping.js

--- src/commands/info/ping.js
+++ src/commands/info/ping.js
@@ -2,10 +2,6 @@ const {EmbedBuilder, PermissionsBitField} = require('discord.js');
 const {SlashCommandBuilder} = require('@discordjs/builders');
 
 exports.commandBase = {
-	prefixData: {
-		name: 'ping',
-		aliases: ['pong'],
-	},
 	slashData: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Pong!'),
