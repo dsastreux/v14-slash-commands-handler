f764ea2fdb7b9ae1957997266d67869b8b03d643
dsastreux <167467152+dsastreux@users.noreply.github.com>
2024-05-19 13:17:17 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	prefix: '&',
-	owner: '1225515354046992405',
-	token: 'MTIzODQ0ODU3MzYwNDE3MTgxNg.G5zsR3.rSPcQm9O1_29jEbR0ZgPsRb7ktLZVhu6vhMF8E',
+	owner: 'owner_id_here',
+	token: 'token_here',
 };
